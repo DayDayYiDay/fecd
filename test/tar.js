9e5d12a322bfb1f66e447c42ec199b89ce16202a
DayYiDay Day <1964406718@qq.com>
2019-05-16 19:04:08 +0800

    fix test

--- test/tar.js
+++ test/tar.js
@@ -9,7 +9,7 @@ const expect = chai.expect;
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      tarDir('test.tar.gz', (tarName, file) => {
+      tarDir('dist', 'test.tar.gz', (tarName, file) => {
         var stats = fs.statSync('test.tar.gz');
         var fileSizeInBytes = stats["size"]
         expect(fileSizeInBytes).to.be.above(0)
